47e2d0ee5172d67751d70e5173f9995a3f41b752
Grzegorz Michrowski <grzegorz.michrowski@gmail.com>
2019-02-09 00:33:37 +0100

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/codecool/Desktop/workspace/Porfolio_Lab/Oddaj_rzeczy/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ oddaj_rz/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OddajRzConfig(AppConfig):
+    name = 'oddaj_rz'
