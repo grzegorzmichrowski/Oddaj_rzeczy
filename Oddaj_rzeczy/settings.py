7f31b3b6d358c11289a67988d2fadfe2cb5f42d8
Grzegorz Michrowski <grzegorz.michrowski@gmail.com>
2019-02-09 00:40:59 +0100

    Database configuration

--- Oddaj_rzeczy/settings.py
+++ Oddaj_rzeczy/settings.py
@@ -72,17 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Oddaj_rzeczy.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -120,3 +109,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from Oddaj_rzeczy.local_settings import DATABASES
+except ModuleNotFoundError:
+    print("Brak konfiguracji bazy danych w pliku local_settings.py!")
+    print("Uzupełnij dane i spróbuj ponownie!")
+    exit(0)
