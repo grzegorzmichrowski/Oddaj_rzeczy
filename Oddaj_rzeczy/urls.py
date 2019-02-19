16e4593b4fb82c454d3e3e9fa40390fe025df04d
Grzegorz Michrowski <grzegorz.michrowski@gmail.com>
2019-02-19 06:08:00 +0100

    Login added

--- Oddaj_rzeczy/urls.py
+++ Oddaj_rzeczy/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from oddaj_rz.views import LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LandingPageView.as_view(), name="landing_page")
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', LandingPageView.as_view(), name="landing_page"),
 ]
