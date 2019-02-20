67159a26922dca23886e8618d6eab8de8aa6a467
Grzegorz Michrowski <grzegorz.michrowski@gmail.com>
2019-02-20 15:48:06 +0100

    User registration added

--- Oddaj_rzeczy/urls.py
+++ Oddaj_rzeczy/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from oddaj_rz.views import LandingPageView, LoginView, LogoutView
+from oddaj_rz.views import LandingPageView, LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name="landing_page"),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
+    path('register', RegisterView.as_view(), name='register'),
 ]
 
 
--- oddaj_rz/views.py
+++ oddaj_rz/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -36,3 +37,40 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('landing_page')
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        username = request.POST.get("login")
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('password2')
+        users = User.objects.all()
+        usernames = []
+        emails = []
+        for i in users:
+            emails.append(i.email)
+            usernames.append(i.username)
+        if name and surname and username and email and password and confirmPassword and password == confirmPassword:
+            if username in usernames:
+                text = 'Podany użytkownik już istnieje'
+                return render(request, 'register.html', {"text": text})
+            elif email in emails:
+                text = 'Podany email już istnieje'
+                return render(request, 'register.html', {"text": text})
+            else:
+                User.objects.create_user(first_name=name,
+                                         last_name=surname,
+                                         username=username,
+                                         email=email,
+                                         password=password,
+                                         )
+                return redirect('login')
+        text = 'Hasła niezgodne'
+        return render(request, 'register.html', {"text": text})
